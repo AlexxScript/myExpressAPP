a4ec1ab87bb90034603032f4cb0474ffdd09c4f7
AlexxScript <ameneyroalex@gmail.com>
2023-11-23 12:28:49 -0600

    Token verify

--- server/src/controllers/homeController.js
+++ server/src/controllers/homeController.js
@@ -2,17 +2,12 @@ import { pool } from "../libs/conection.js";
 
 export const getHome = async (req, res) => {
     const client = await pool.connect();
-    client.query("SELECT * FROM users")
-        .then(res => {
-            console.log(res.rows)
-            client.release()
-        })
-        .catch(err => {
-            console.log(err)
-            client.release()
-
-        });
+    const data = await client.query("SELECT * FROM tasks WHERE id_user=$1",[req.user.id_user]);
+    if (data.rows[0] == null) {
+        return res.status(404).json({message:"No tasks"});        
+    }
+    console.log(data)
     res.json({
-        message: "server is running"
+        message: data
     });
 }
--- server/src/middlewares/authToken.js
+++ server/src/middlewares/authToken.js
@@ -1,4 +1,16 @@
+import jwt from "jsonwebtoken";
+
 export const authToken = (req, res, next) => {
-    console.log(req.cookies.token);
-    next();
+    const {token} = req.cookies;
+
+    console.log(token)
+
+    if(!token) return res.status(401).json({message:"No token provied"});
+
+    jwt.verify(token,process.env.JWT_SECRET,(err,user)=>{
+        if (err) return res.status(403).json({message:"Invalid token"});
+        req.user = user;
+        next();
+    });
+    // console.log(req.cookies.token);
 }
